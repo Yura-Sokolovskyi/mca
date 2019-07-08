7070d96e4ae5f579adfd1a9c022fdbca4d67e84e
s-y-r@ukr.net <yurasokil060592>
2019-07-08 17:46:14 +0300

    add static path

---
+++ src/main/java/yurii/sokolovskyi/mca/config/StaticResourcesConfiguration.java
@@ -0,0 +1,24 @@
+package yurii.sokolovskyi.mca.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
+
+import static yurii.sokolovskyi.mca.service.FileService.IMG_DIR;
+
+@Configuration
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class StaticResourcesConfiguration implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations(Paths.get(IMG_DIR).toUri().toString());
+    }
+
+}
